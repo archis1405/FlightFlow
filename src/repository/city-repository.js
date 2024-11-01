32695a72dd0aab7104efaff1140ef0aebeb598a4
Archisman <archismandas1405@gmail.com>
2024-11-01 16:13:12 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,7 +3,10 @@ const { City } = require('../models/index');
 class CityRepository{
     async createCity({name}){
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
+            return city ;
         } 
         catch (error) {
             throw {error};
