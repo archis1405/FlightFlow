be6562d092aa3187c85acc003ab9ffd1aaeca0aa
Archisman <archismandas1405@gmail.com>
2024-11-02 13:06:38 +0530

    Implemented all the controllers

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -29,27 +29,67 @@ const create = async (req,res) => {
     }
 }
 
-const destroy = (req,res) => {
+// DELETE -> /city/:id
+const destroy = async (req,res) => {
     try {
-        
+        const response = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data : city ,
+            success : true,
+            message : 'Successfully deleted a city' ,
+            err: {}
+        });
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to delete the city',
+            err: error
+        });
     }
 }
 
-const get = (req,res) => {
+// GET -> /city/:id
+const get = async (req,res) => {
     try {
-        
+        const response = await cityService.getCity(req.params.id);
+        return res.status(200).json({
+            data : response ,
+            success : true,
+            message : 'Successfully fetched a city' ,
+            err: {}
+        });
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to get city',
+            err: error
+        });
     }
 }
 
-const update = (req,res) => {
+// PATCH -> /city/:id -> req.body
+const update = async (req,res) => {
     try {
-        
+        const response = await cityService.updateCity(req.params.id , req.body);
+        return res.status(200).json({
+            data : response ,
+            success : true,
+            message : 'Successfully fetched a city' ,
+            err: {}
+        });
     } catch (error) {
         console.log(error);
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the city',
+            err: error
+        });
     }
 }
 
